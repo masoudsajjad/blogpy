c2a9355e1e88718348baa1969bde56d2edc22183
masoud <masoud67130@gmail.com>
2020-05-16 22:01:12 +0430

    dynamin slider

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns= [
-    url(r'^$', views.IndexPage.as_view(), name='index')
+    url(r'^$', views.IndexPage.as_view(), name='index'),
+    url(r'^contact/$', views.ContactPage.as_view(), name='contact'),
+    url(r'^about/$', views.AboutPage.as_view(), name='about'),
+    url(r'^category/$', views.CategoryPage.as_view(), name='category'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,31 @@ class IndexPage(TemplateView):
                 'category': article.category.title,
                 'created_at': article.created_at,
             })
+
+        promote_data = []
+        all_promote_article = Article.objects.filter(promote=True)
+        for promote_article in all_promote_article:
+            promote_data.append({
+                'category': promote_article.category.title,
+                'title': promote_article.title,
+                'cover': promote_article.cover.url,
+            }
+            )
         context = {
             'article_data': article_data,
+            'promote_article_data': promote_data
         }
         return render(request, 'index.html', context)
+
+
+class ContactPage(TemplateView):
+    template_name = 'page-contact.html'
+
+
+class AboutPage(TemplateView):
+    template_name = 'page-about.html'
+
+
+class CategoryPage(TemplateView):
+    template_name = 'category.html'
+
